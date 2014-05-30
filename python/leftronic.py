ce53ce80eb66dd570c93a4d80dd60444a7ffbc35
csmithd <csmithd@gmail.com>
2014-05-30 14:52:59 -0700

    PEP8 variable name updating

--- python/leftronic.py
+++ python/leftronic.py
@@ -18,13 +18,13 @@ class Leftronic(object):
     Provides access to Leftronic Custom Data API
     """
 
-    def __init__(self, authKey):
-        self.accessKey = authKey
-        self.apiUrl = 'https://www.leftronic.com/customSend'
-        self.cryptoKey = None
+    def __init__(self, auth_key):
+        self.access_key = auth_key
+        self.api_url = 'https://www.leftronic.com/customSend'
+        self.crypto_key = None
 
-    def setEncryptionKey(self, cryptoKey):
-        self.cryptoKey = cryptoKey
+    def setEncryptionKey(self, crypto_key):
+        self.crypto_key = crypto_key
 
     def pushMultiple(self, points):
         """
@@ -39,14 +39,14 @@ class Leftronic(object):
         else:
             return self.postData({"streams": points})
 
-    def pushNumber(self, streamName, point):
+    def pushNumber(self, stream_name, point):
         """
         Pushes a number (or list of numbers) to a Number, Horizontal/Vertical
         Bar, Dial widget, Stoplight or sparkline/line graph
         """
-        return self.postData(self.populateNumber(streamName, point))
+        return self.postData(self.populateNumber(stream_name, point))
 
-    def populateNumber(self, streamName, point):
+    def populateNumber(self, stream_name, point):
         """
         Formats the provided number, list of numbers, or dict of
         numbers as a Python dict, e.g., to be pushed along with other
@@ -67,9 +67,9 @@ class Leftronic(object):
         else:
             raise TypeError('You must pass in a numeric value, a list, or a '
                             'dict')
-        return {'streamName': streamName, 'point': point}
+        return {'streamName': stream_name, 'point': point}
 
-    def pushGeo(self, streamName, lati, longi, color=None):
+    def pushGeo(self, stream_name, lat, lon, color=None):
         """
         Pushes a geographic location (latitude and longitude) to a Map
         widget.  Color can also be passed (red, green, blue, yellow,
@@ -77,9 +77,9 @@ class Leftronic(object):
 
         Default color is red.
         """
-        return self.postData(self.populateGeo(streamName, lati, longi, color))
+        return self.postData(self.populateGeo(stream_name, lat, lon, color))
 
-    def populateGeo(self, streamName, lati, longi, color=None):
+    def populateGeo(self, stream_name, lat, lon, color=None):
         """
         Formats a geographic location (latitude and longitude) as a
         dict, e.g., to be supplied to 'pushMultiple()` Color can also
@@ -87,96 +87,97 @@ class Leftronic(object):
 
         Default color is red.
         """
-        if type(lati) != list and type(longi) != list and type(color) != list:
-            point = {'latitude': lati, 'longitude': longi}
+        if type(lat) != list and type(lon) != list and type(color) != list:
+            point = {'latitude': lat, 'longitude': lon}
             if color:
                 point['color'] = color
 
-        elif type(lati) == list and type(longi) == list:
-            if len(lati) != len(longi):
+        elif type(lat) == list and type(lon) == list:
+            if len(lat) != len(lon):
                 raise ValueError('Your lists of latitudes and longitudes must '
                                  'be the same size.')
             point = []
-            for i in range(len(lati)):
-                obj = {'latitude': lati[i], 'longitude': longi[i]}
+            for i in range(len(lat)):
+                obj = {'latitude': lat[i], 'longitude': lon[i]}
                 if color and type(color) == list and color[i]:
                     obj['color'] = color[i]
                 point.append(obj)
         else:
             raise TypeError('Your latitude, longitude, or color were not '
                             'properly formatted.')
-        return {'streamName': streamName, 'point': point}
+        return {'streamName': stream_name, 'point': point}
 
-    def pushText(self, streamName, myTitle, myMsg, imgUrl=None):
+    def pushText(self, stream_name, my_title, my_msg, img_url=None):
         """
         Pushes a title and message to a Text Feed widget.
         """
-        return self.postData(self.populateText(streamName, myTitle,
-                                               myMsg, imgUrl))
+        return self.postData(self.populateText(stream_name, my_title,
+                                               my_msg, img_url))
 
-    def populateText(self, streamName, myTitle, myMsg, imgUrl=None):
+    def populateText(self, stream_name, my_ttle, my_msg, img_url=None):
         """
         Formats a title and message for a Text Feed widget as a dict,
         which can be passed to `pushMultiple()`
         """
-        if (type(myTitle) != list and
-                type(myMsg) != list and
-                type(imgUrl) != list):
-            point = {'title': myTitle, 'msg': myMsg}
-            if imgUrl:
-                point['imgUrl'] = imgUrl
-        elif type(myTitle) == list and type(myMsg) == list:
-            if len(myTitle) != len(myMsg):
+        if (type(my_ttle) != list and
+                type(my_msg) != list and
+                type(img_url) != list):
+            point = {'title': my_ttle, 'msg': my_msg}
+            if img_url:
+                point['imgUrl'] = img_url
+        elif type(my_ttle) == list and type(my_msg) == list:
+            if len(my_ttle) != len(my_msg):
                 raise ValueError
             point = []
-            for i in range(len(myTitle)):
-                obj = {'title': myTitle[i], 'msg': myMsg[i]}
-                if imgUrl and type(imgUrl) == list and imgUrl[i]:
-                    obj['imgUrl'] = imgUrl[i]
+            for i in range(len(my_ttle)):
+                obj = {'title': my_ttle[i], 'msg': my_msg[i]}
+                if img_url and type(img_url) == list and img_url[i]:
+                    obj['imgUrl'] = img_url[i]
                 point.append(obj)
         else:
             raise TypeError
 
-        return {'streamName': streamName, 'point': point}
+        return {'streamName': stream_name, 'point': point}
 
-    def pushLeaderboard(self, streamName, leaderArray):
+    def pushLeaderboard(self, stream_name, leader_array):
         """ Pushes an array to a Leaderboard widget. """
-        return self.postData(self.populateLeaderboard(streamName, leaderArray))
+        return self.postData(self.populateLeaderboard(stream_name,
+                                                      leader_array))
 
-    def populateLeaderboard(self, streamName, leaderArray):
+    def populateLeaderboard(self, stream_name, leader_array):
         """
         Formats a leaderboard array as a dict for, e.g., passing to
         `pushMultiple()`
         """
-        return {'streamName': streamName,
+        return {'streamName': stream_name,
                 'point': {
-                    'leaderboard': leaderArray
+                    'leaderboard': leader_array
                 }}
 
-    def pushList(self, streamName, listArray):
+    def pushList(self, stream_name, list_array):
         """ Pushes an array to a List widget. """
-        return self.postData(self.populateList(streamName, listArray))
+        return self.postData(self.populateList(stream_name, list_array))
 
-    def populateList(self, streamName, listArray):
+    def populateList(self, stream_name, list_array):
         """
         Formats a list as a dict to be pushed using, e.g., `pushMultiple()`.
         """
-        if type(listArray) != list:
+        if type(list_array) != list:
             raise TypeError
-        for i in range(len(listArray)):
-            if type(listArray[i]) != dict:
-                listArray[i] = {'listItem': listArray[i]}
+        for i in range(len(list_array)):
+            if type(list_array[i]) != dict:
+                list_array[i] = {'listItem': list_array[i]}
 
-        return {'streamName': streamName,
+        return {'streamName': stream_name,
                 'point': {
-                    'list': listArray
+                    'list': list_array
                 }}
 
-    def pushPair(self, streamName, x, y):
+    def pushPair(self, stream_name, x, y):
         """ Pushes an x,y pair to a Pair widget"""
-        return self.postData(self.populatePair(streamName, x, y))
+        return self.postData(self.populatePair(stream_name, x, y))
 
-    def populatePair(self, streamName, x, y):
+    def populatePair(self, stream_name, x, y):
         """
         Formats an x, y pair (or lists of x, y pairs) as
         """
@@ -188,57 +189,57 @@ class Leftronic(object):
                 point.append({'x': x[i], 'y': y[i]})
         else:
             point = {'x': x, 'y': y}
-        return {'streamName': streamName,
+        return {'streamName': stream_name,
                 'point': point}
 
-    def pushImage(self, streamName, imgUrl):
+    def pushImage(self, stream_name, img_url):
         """ Pushes an image to an Image widget"""
-        return self.postData(self.populateImage(streamName, imgUrl))
+        return self.postData(self.populateImage(stream_name, img_url))
 
-    def populateImage(self, streamName, imgUrl):
+    def populateImage(self, stream_name, img_url):
         """
         Formats a provided image url as a dict to be pushed using, e.g.,
         `pushMultiple()`
         """
-        point = {'imgUrl': imgUrl}
-        return {'streamName': streamName,
+        point = {'imgUrl': img_url}
+        return {'streamName': stream_name,
                 'point': point}
 
-    def pushLabel(self, streamName, label):
+    def pushLabel(self, stream_name, label):
         """ Pushes a label to a Label widget"""
-        return self.postData(self.populateLabel(streamName, label))
+        return self.postData(self.populateLabel(stream_name, label))
 
-    def populateLabel(self, streamName, label):
+    def populateLabel(self, stream_name, label):
         """
         Formats a label as a dict
         """
         point = {'label': label}
-        return {'streamName': streamName,
+        return {'streamName': stream_name,
                 'point': point}
 
-    def pushTable(self, streamName, headerRow, dataRows):
+    def pushTable(self, stream_name, header_row, data_rows):
         """ Pushes a table to a Table widget """
-        return self.postData(self.populateTable(streamName,
-                                                headerRow,
-                                                dataRows))
+        return self.postData(self.populateTable(stream_name,
+                                                header_row,
+                                                data_rows))
 
-    def populateTable(self, streamName, headerRow, dataRows):
-        dataRows.insert(0, headerRow)
-        point = {'table': dataRows}
-        return {'streamName': streamName,
+    def populateTable(self, stream_name, header_row, data_rows):
+        data_rows.insert(0, header_row)
+        point = {'table': data_rows}
+        return {'streamName': stream_name,
                 'point': point}
 
-    def clear(self, streamName):
-        parameters = {'streamName': streamName,
+    def clear(self, stream_name):
+        parameters = {'streamName': stream_name,
                       'command': 'clear'}
         return self.postData(parameters)
 
     def postData(self, parameters):
         """ Makes an HTTP POST to the API URL. """
         # add the access key
-        parameters['accessKey'] = self.accessKey
+        parameters['accessKey'] = self.access_key
 
-        if self.cryptoKey:
+        if self.crypto_key:
             self.encryptStreams(parameters)
 
         # Convert to JSON
@@ -246,13 +247,13 @@ class Leftronic(object):
 
         logger.debug(jsonData)
         # Make request
-        response = urllib2.urlopen(self.apiUrl, jsonData)
+        response = urllib2.urlopen(self.api_url, jsonData)
         return response.read()
 
     def encryptText(self, text):
         # set up AES encryption
         iv = Random.get_random_bytes(16)
-        key = self.cryptoKey
+        key = self.crypto_key
         # pad key with spaces if its length is not a multiple of 16
         # note that for extra security the user should not choose a short key
         if len(key) % 16 != 0:
